78a9360b0ac10bd4bf6ab0b74af8c1dcc163fbe2
amitkarmakar07 <karamit819@gmail.com>
2023-07-24 19:29:47 +0530

    Update server.php

--- datalayer/server.php
+++ datalayer/server.php
@@ -4,7 +4,7 @@
 session_start();
 $errors=array();
 
-$mysqli = new mysqli("localhost","fuck amit","","registration");
+$mysqli = new mysqli("localhost","hello world","","registration");
 
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
