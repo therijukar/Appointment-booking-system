40e1dcc445eb96e47b00792100f0cfe08829e94d
Sandipan <sarkarsandipan423@gmail.com>
2023-07-25 00:38:08 +0530

    Update feedback.php

--- presentaionlayer/admin/feedback.php
+++ presentaionlayer/admin/feedback.php
@@ -11,40 +11,16 @@
 <header>
 	<h1>Doctor<span>Patient</span></h1>
 		<nav>
-		
-
-
-		
 		<ul> 
-			
-		
-		
 			<li><a href="index3.php">Add/Delete Doctor</a></li>
 			<li><a href="viewdoctor.php">View Doctors</a></li>
 			<li><a href=" viewpatients.php">View Patients</a></li>
 			<li><a href="viewappointments.php">View Appointments</a></li>
 			<li><a href="searchdonoradmin.php">Search Donor</a></li>
 						<li><a href="feedback.php">FeedBack</a></li>
-
-
 			<li><a href="../../applicationlayer/Doctorpatient.php">Logout</a></li>
-
-			
-
-
-	
-			
-
 		</ul>
-		
-
-
-
 	</nav>
-
-
-
-
 </header>
 
 <body>
@@ -54,27 +30,16 @@
 		<th>Patient ID</th>
 		<th>Patient Name</th>
 		<th>FeedBack</th>
-		
-
 		</tr>
-
 		<?php $sql3="SELECT  * FROM  patients,feedback WHERE pID=UserID " ;
 		$result3=$mysqli->query($sql3);
 		if(mysqli_num_rows($result3)>=1){
 			while ($row3=$result3->fetch_assoc()) {
-
 				echo "<tr><td>".$row3["pID"]."</td><td>".$row3["Name"]."</td><td>".$row3['feedback']."</td></tr>";
 			}
-
-
 			echo "</table";
-	
-
-
 		}
-
 		?>
-		
 	</table>
 </body>
 </html>
